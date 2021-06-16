f77b13199c02f5c104b2f39a7f80b788ff9b8829
prashant <pksingh706586@gmail.com>
2021-06-16 20:48:21 +0530

    hover affect

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.10em solid #fff}";
   document.body.appendChild(css);
-};
+ };
 
